1f0828ed95969f042c0dae3ffcc6fa71f33a104b
BRAYANPISSO02 <125114562+BRAYANPISSO02@users.noreply.github.com>
2024-09-09 19:37:53 -0500

    Creamos la función estatus_hz

--- Core/Src/led_estatus.c
+++ Core/Src/led_estatus.c
@@ -1,7 +1,14 @@
 #include "led_estatus.h"
+#include "main.h"
 
 void estatus_hz (uint8_t stt){
 
+	uint16_t last_press_tick = 0;
+	if (stt == 1 && HAL_GetTick() > (last_press_tick + 125)) {
 
+			HAL_GPIO_TogglePin(SYSTEM_LED_GPIO_Port, SYSTEM_LED_Pin);
+			last_press_tick = HAL_GetTick();
+	}else{
 
+	}
 }
